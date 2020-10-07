2f04cf0b32b8bcef196a6c95f52d77130898096e
ozzhaas <kellenhaas@outlook.com>
2020-10-07 18:17:38 -0400

    10.7 6:17pm

---
+++ lab7/src/cpsc2150/banking/AbsCustomer.java
@@ -0,0 +1,31 @@
+/*Kellen Haas
+  CPSC 2150
+  lab7
+  10/07/20
+ */
+
+package cpsc2150.banking;
+
+public abstract class AbsCustomer implements ICustomer {
+
+    //Every customer will have an IMortgage Loan
+    //Protected so the child classes can access it
+    protected IMortgage loan;
+
+    @Override
+    public String toString()
+    {
+        String str = "";
+        str += "Name: " + getName() + "\n";
+        str += "Income: $" + getIncome() + "\n";
+        str += "Credit Score: " + getCreditScore() + "\n";
+        str += "Monthly Debt: $" + getMonthlyDebtPayments() + "\n";
+        str += "Mortgage info: \n";
+        if( appliedForLoan()) {
+            str += loan.toString();
+        }
+
+        return str;
+
+    }
+}
